67f8076dc1e4d8ef5b21621cf916adc437cec7d0
Brian-mula <mulatiwangilabrian@gmail.com>
2022-10-07 03:00:04 +0300

    completed login page

---
+++ lib/widgets/input_field.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class CustomeInput extends StatelessWidget {
+  final TextEditingController controller;
+  final TextInputType inputType;
+  final bool? obScuretext;
+  final String labelText;
+  final IconData icon;
+  const CustomeInput(
+      {super.key,
+      required this.controller,
+      required this.icon,
+      required this.labelText,
+      required this.inputType,
+      this.obScuretext});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 40,
+      child: TextFormField(
+        controller: controller,
+        keyboardType: inputType,
+        obscureText: obScuretext ?? false,
+        decoration: InputDecoration(
+            labelText: labelText,
+            prefixIcon: Icon(icon),
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(6),
+                borderSide: BorderSide(color: Colors.blue.shade600)),
+            focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(6),
+                borderSide: BorderSide(color: Colors.orange.shade600))),
+      ),
+    );
+  }
+}
