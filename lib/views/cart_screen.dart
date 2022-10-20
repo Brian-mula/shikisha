b5a6aeef14fc64fb84508f636516a8e6f707f07d
Brian-mula <mulatiwangilabrian@gmail.com>
2022-10-20 08:37:29 +0300

    completed cart page

--- lib/views/cart_screen.dart
+++ lib/views/cart_screen.dart
@@ -25,20 +25,84 @@ class _CartScreenState extends ConsumerState<CartScreen> {
       ),
       body: Container(
         padding: const EdgeInsets.only(top: 10, right: 10, left: 10),
-        child: ListView.builder(
-            itemCount: 10,
-            itemBuilder: (context, index) => Container(
-                  margin: const EdgeInsets.only(bottom: 10),
-                  child: ListTile(
-                    leading: Container(
-                      height: 100,
-                      width: 100,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(6),
-                          color: Colors.blue),
-                    ),
-                  ),
-                )),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 700,
+              child: ListView.builder(
+                  itemCount: 10,
+                  itemBuilder: (context, index) => Container(
+                        margin: const EdgeInsets.only(bottom: 10),
+                        child: ListTile(
+                          leading: Container(
+                            height: 100,
+                            width: 100,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(6),
+                                image: const DecorationImage(
+                                    image: NetworkImage(
+                                        'https://images.unsplash.com/photo-1661961110218-35af7210f803?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxzZWFyY2h8NTd8fGxhcHRvcHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'),
+                                    fit: BoxFit.cover)),
+                          ),
+                          title: TextWidget(
+                            text: "Product Tile",
+                            textStyle: theme.textTheme.headline6!
+                                .copyWith(color: Colors.black45),
+                          ),
+                          subtitle: TextWidget(
+                            text: "Ksh. 3000",
+                            textStyle: theme.textTheme.headline6!.copyWith(
+                                fontSize: 16, color: Colors.orange.shade900),
+                          ),
+                          trailing: SizedBox(
+                            width: 130,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                IconButton(
+                                    onPressed: () {},
+                                    icon: const Icon(Icons.add)),
+                                const SizedBox(
+                                  width: 5,
+                                ),
+                                TextWidget(
+                                  text: "4",
+                                  textStyle: theme.textTheme.headline6!
+                                      .copyWith(fontSize: 16),
+                                ),
+                                const SizedBox(
+                                  width: 5,
+                                ),
+                                IconButton(
+                                    onPressed: () {},
+                                    icon: const Icon(Icons.remove)),
+                              ],
+                            ),
+                          ),
+                        ),
+                      )),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.orange.shade900)),
+                    onPressed: () {},
+                    child: TextWidget(
+                      text: "Checkout",
+                      textStyle: theme.textTheme.headline6!
+                          .copyWith(color: Colors.white),
+                    )),
+                TextWidget(
+                  text: "Ksh. 3000",
+                  textStyle: theme.textTheme.headline6,
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
