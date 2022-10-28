aced31501b128e1015b3b0f187a8e9a940007673
Brian-mula <mulatiwangilabrian@gmail.com>
2022-10-28 04:31:09 +0300

    added firebase function

--- lib/logic/providers/products_provider.dart
+++ lib/logic/providers/products_provider.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shikisha/logic/models/products_model.dart';
 import 'package:shikisha/logic/repos/cart_repo.dart';
+import 'package:shikisha/logic/repos/firebase.dart';
 import 'package:shikisha/logic/repos/products_repo.dart';
 
 final productsProvider = FutureProvider<List<Product>>((ref) async {
@@ -13,4 +14,8 @@ final productProvider = ChangeNotifierProvider<ProductsRepo>((ref) {
 
 final cartProvider = Provider<CartRepo>((ref) {
   return CartRepo();
+});
+
+final firebaseProvider = Provider<SellerProduct>((ref) {
+  return SellerProduct();
 });
